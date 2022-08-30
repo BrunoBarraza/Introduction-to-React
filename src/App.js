852c718399e57025e61abc90ba288d1589470945
brunobarraza <bruno.barraza@atos.net>
2022-08-30 15:31:05 -0500

    Interactive components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-const estilo2 = {
-  boxShadow: "0 5px 3px rgba(0,0,0,0.5)",
-};
-
-const estilo = ({ bg = "#222" }) => ({
-  backgroundColor: bg,
-  color: "#fff",
-  padding: "10px 15px",
-  margin: "10px 15px",
-});
-
-const Li = ({ children }) => {
-  return (
-    <li style={{ ...estilo2, ...estilo({ bg: "#333" }) }} className="clase-li">
-      {children}
-    </li>
-  );
-};
+import Button from "./Button";
 
 const App = () => {
-  const valor = "triste";
   return (
-    <ul style={estilo({ bg: "#750" })} className="clase-css">
-      <Li estado="feliz">valor de li</Li>
-    </ul>
+    <div>
+      <h1 onClick={(e) => console.log('click', e)}>Hola mundo!</h1>
+      <Button onClick={() => console.log("clickeado")}>Enviar</Button>
+    </div>
   );
 };
 
